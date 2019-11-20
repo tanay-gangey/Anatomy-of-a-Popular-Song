b8ae03c1c92c02c2ccf086ea06c6831a28987aa3
Karan P <karan20000000000@gmail.com>
2019-11-20 15:59:44 +0530

    Add generalized tree for countries

---
+++ tree_multiple_countries.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from sklearn import tree
+import pandas as pd
+import numpy as np
+
+def mape(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return (np.mean(np.abs((y_true - y_pred)/y_true)) * 100)
+
+
+def train(country):
+    data = pd.read_csv(f"normdata/{country}_waf.csv")
+    data1=pd.read_csv(f"norm-age-data/{country}.csv")
+    age=data1['Age on Chart']
+    
+    data["Age_on_Chart"]=age
+    data=data.drop(['Unnamed: 0', 'Track Name', 'Artist', 'URL', 'Year','Month','Day','Region'], axis = 1)
+    
+    x_train, y_train = data.drop('Position', axis = 1) , data['Position']
+    clf = tree.DecisionTreeRegressor(max_depth = 10, min_samples_split = 30)
+    clf = clf.fit(x_train, y_train)
+    
+    print(clf.tree_.node_count)
+    print(clf.tree_.max_depth)
+
+    return clf
+
+def test(country, model):
+    data = pd.read_csv(f"normdata/{country}_waf.csv")
+    data1=pd.read_csv(f"norm-age-data/{country}.csv")
+    age=data1['Age on Chart']
+    
+    data["Age_on_Chart"]=age
+    data=data.drop(['Unnamed: 0', 'Track Name', 'Artist', 'URL', 'Year','Month','Day','Region'], axis = 1)
+    
+    x_test, y_test = data.drop('Position', axis = 1), data['Position']
+    
+    y_pred = model.predict(x_test)
+    
+    return mape(y_test, y_pred)
---
+++ tree_self_country.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+
+from sklearn import tree
+import pandas as pd
+import numpy as np
+
+def mape(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return (np.mean(np.abs((y_true - y_pred)/y_true)) * 100)
+
+
+def acc(country):
+    data = pd.read_csv(f"normdata/{country}_waf.csv")
+    data1=pd.read_csv(f"norm-age-data/{country}.csv")
+    age=data1['Age on Chart']
+    
+    data["Age_on_Chart"]=age
+    data=data.drop(['Unnamed: 0', 'Track Name', 'Artist', 'URL', 'Year','Month','Day','Region'], axis = 1)
+    
+    data_train = data.iloc[0:15100]
+    data_test = data.iloc[15100:]
+    
+    x_train, y_train = data_train.drop('Position', axis = 1) , data_train['Position']
+    x_test, y_test = data_test.drop('Position', axis = 1), data_test['Position']
+
+    clf = tree.DecisionTreeRegressor(max_depth = 10, min_samples_split = 30)
+    clf = clf.fit(x_train, y_train)
+    
+    print(clf.tree_.node_count)
+    print(clf.tree_.max_depth)
+
+    y_pred = clf.predict(x_test)
+    
+    print(100-mape(y_test, y_pred))
