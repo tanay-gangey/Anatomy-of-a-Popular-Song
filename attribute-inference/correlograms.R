09f042f582b01386c1dce378954ee04534c01fe6
Tanay Gangey <tanay.gangey@gmail.com>
2019-11-21 17:53:26 +0530

    organized repo and added spotify scraping script

---
+++ attribute-inference/correlograms.R
@@ -0,0 +1,27 @@
+library(corrgram)
+library(ellipse)
+
+path = "path/to/data/"
+
+us = read.csv(paste0(path, "us_waf.csv"))
+au = read.csv(paste0(path, "au_waf.csv"))
+gb = read.csv(paste0(path, "gb_waf.csv"))
+jp = read.csv(paste0(path, "jp_waf.csv"))
+ph = read.csv(paste0(path, "ph_waf.csv"))
+
+nc = c(1.10,11,12,13,15,16,17,18,19,20,21)
+
+us_cor = cor(us[,nc])
+plotcorr(us_cor, main = "US Correlogram", col = "blue", type = "lower")
+
+au_cor = cor(au[,nc])
+plotcorr(au_cor, main = "AU Correlogram", col = "blue", type = "lower")
+
+gb_cor = cor(gb[,nc])
+plotcorr(gb_cor, main = "GB Correlogram",  col = "blue", type = "lower")
+
+jp_cor = cor(jp[,nc])
+plotcorr(jp_cor, main = "Japan Correlogram",  col = "blue", type = "lower")
+
+ph_cor = cor(ph[,nc])
+plotcorr(ph_cor, main = "Phillipines Correlogram",  col = "blue", type = "lower")
